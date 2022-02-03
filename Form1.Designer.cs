81e2c3e5e8f522c98c58f91d8f2475878a249fd3
rlawlswo128 <80729134+rlawlswo128@users.noreply.github.com>
2022-02-03 18:29:47 +0900

    update

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,13 +29,122 @@ namespace Csharp_Component
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.variable1 = new System.Windows.Forms.TextBox();
+            this.variable2 = new System.Windows.Forms.TextBox();
+            this.add = new System.Windows.Forms.Button();
+            this.subtrack = new System.Windows.Forms.Button();
+            this.multiply = new System.Windows.Forms.Button();
+            this.divide = new System.Windows.Forms.Button();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.resultListView = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // variable1
+            // 
+            this.variable1.Location = new System.Drawing.Point(39, 35);
+            this.variable1.Name = "variable1";
+            this.variable1.Size = new System.Drawing.Size(262, 21);
+            this.variable1.TabIndex = 0;
+            // 
+            // variable2
+            // 
+            this.variable2.Location = new System.Drawing.Point(39, 87);
+            this.variable2.Name = "variable2";
+            this.variable2.Size = new System.Drawing.Size(262, 21);
+            this.variable2.TabIndex = 1;
+            // 
+            // add
+            // 
+            this.add.Font = new System.Drawing.Font("굴림", 14F);
+            this.add.Location = new System.Drawing.Point(39, 137);
+            this.add.Name = "add";
+            this.add.Size = new System.Drawing.Size(61, 51);
+            this.add.TabIndex = 2;
+            this.add.Text = "+";
+            this.add.UseVisualStyleBackColor = true;
+            this.add.Click += new System.EventHandler(this.add_Click);
+            // 
+            // subtrack
+            // 
+            this.subtrack.Font = new System.Drawing.Font("굴림", 14F);
+            this.subtrack.Location = new System.Drawing.Point(106, 137);
+            this.subtrack.Name = "subtrack";
+            this.subtrack.Size = new System.Drawing.Size(61, 51);
+            this.subtrack.TabIndex = 2;
+            this.subtrack.Text = "-";
+            this.subtrack.UseVisualStyleBackColor = true;
+            this.subtrack.Click += new System.EventHandler(this.subtrack_Click);
+            // 
+            // multiply
+            // 
+            this.multiply.Font = new System.Drawing.Font("굴림", 14F);
+            this.multiply.Location = new System.Drawing.Point(173, 137);
+            this.multiply.Name = "multiply";
+            this.multiply.Size = new System.Drawing.Size(61, 51);
+            this.multiply.TabIndex = 2;
+            this.multiply.Text = "*";
+            this.multiply.UseVisualStyleBackColor = true;
+            this.multiply.Click += new System.EventHandler(this.multiply_Click);
+            // 
+            // divide
+            // 
+            this.divide.Font = new System.Drawing.Font("굴림", 14F);
+            this.divide.Location = new System.Drawing.Point(240, 137);
+            this.divide.Name = "divide";
+            this.divide.Size = new System.Drawing.Size(61, 51);
+            this.divide.TabIndex = 2;
+            this.divide.Text = "/";
+            this.divide.UseVisualStyleBackColor = true;
+            this.divide.Click += new System.EventHandler(this.divide_Click);
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.Location = new System.Drawing.Point(39, 214);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(262, 39);
+            this.resultLabel.TabIndex = 3;
+            this.resultLabel.Text = "결과";
+            this.resultLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // resultListView
+            // 
+            this.resultListView.HideSelection = false;
+            this.resultListView.Location = new System.Drawing.Point(41, 256);
+            this.resultListView.Name = "resultListView";
+            this.resultListView.Size = new System.Drawing.Size(260, 97);
+            this.resultListView.TabIndex = 4;
+            this.resultListView.UseCompatibleStateImageBehavior = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(346, 380);
+            this.Controls.Add(this.resultListView);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.divide);
+            this.Controls.Add(this.multiply);
+            this.Controls.Add(this.subtrack);
+            this.Controls.Add(this.add);
+            this.Controls.Add(this.variable2);
+            this.Controls.Add(this.variable1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox variable1;
+        private System.Windows.Forms.TextBox variable2;
+        private System.Windows.Forms.Button add;
+        private System.Windows.Forms.Button subtrack;
+        private System.Windows.Forms.Button multiply;
+        private System.Windows.Forms.Button divide;
+        private System.Windows.Forms.Label resultLabel;
+        private System.Windows.Forms.ListView resultListView;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -15,6 +15,68 @@ namespace Csharp_Component
         public Form1()
         {
             InitializeComponent();
+            resultListView.View = View.Details;
+            resultListView.Columns.Add("기록", 240);
+        }
+
+        private void add_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int result = Convert.ToInt32(variable1.Text) + Convert.ToInt32(variable2.Text);
+                resultLabel.Text = Convert.ToString(result);
+                ListViewItem item = new ListViewItem(variable1.Text + " + " + variable2.Text + " = " + result);
+                resultListView.Items.Add(item);
+            }
+            catch(Exception)
+            {
+                resultLabel.Text = "입력 값 오류가 발생했습니다.";
+            }
+        }
+
+        private void subtrack_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int result = Convert.ToInt32(variable1.Text) - Convert.ToInt32(variable2.Text);
+                resultLabel.Text = Convert.ToString(result);
+                ListViewItem item = new ListViewItem(variable1.Text + " - " + variable2.Text + " = " + result);
+                resultListView.Items.Add(item);
+            }
+            catch (Exception)
+            {
+                resultLabel.Text = "입력 값 오류가 발생했습니다.";
+            }
+        }
+
+        private void multiply_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int result = Convert.ToInt32(variable1.Text) * Convert.ToInt32(variable2.Text);
+                resultLabel.Text = Convert.ToString(result);
+                ListViewItem item = new ListViewItem(variable1.Text + " * " + variable2.Text + " = " + result);
+                resultListView.Items.Add(item);
+            }
+            catch (Exception)
+            {
+                resultLabel.Text = "입력 값 오류가 발생했습니다.";
+            }
+        }
+
+        private void divide_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int result = Convert.ToInt32(variable1.Text) / Convert.ToInt32(variable2.Text);
+                resultLabel.Text = Convert.ToString(result);
+                ListViewItem item = new ListViewItem(variable1.Text + " / " + variable2.Text + " = " + result);
+                resultListView.Items.Add(item);
+            }
+            catch (Exception)
+            {
+                resultLabel.Text = "입력 값 오류가 발생했습니다.";
+            }
         }
     }
 }
